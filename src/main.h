84242f96b58d9b3f74263754c953bcaeac5530df
PWRcoin <dev@pwr-coin.com>
2018-04-17 16:05:09 +0000

    reducing COIN_YEAR_REWARD to 1% per year

--- src/main.h
+++ src/main.h
@@ -39,7 +39,7 @@ static const unsigned int MAX_INV_SZ = 50000;
 static const int64_t MIN_TX_FEE = 1000;
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64_t MAX_MONEY = 1000000000 * COIN;
-static const int64_t COIN_YEAR_REWARD =  5 * CENT; // 5% per year
+static const int64_t COIN_YEAR_REWARD =  1 * CENT; // 1% per year
 static const int64_t MAX_MINT_PROOF_OF_STAKE = 5 * CENT; // 5% annual interest
 static const int MODIFIER_INTERVAL_SWITCH = 86000;
 
