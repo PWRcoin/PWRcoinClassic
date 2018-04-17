ae4c0ab607c024dff7313b45e7cb89cfb046319b
PWRcoin <dev@pwr-coin.com>
2018-04-17 16:54:58 +0000

    disabling pos check for testnet

--- src/main.cpp
+++ src/main.cpp
@@ -2201,7 +2201,7 @@ bool CBlock::AcceptBlock()
     if (IsProofOfWork() && nHeight > LAST_POW_BLOCK && nHeight < FORK1_BLOCK)
         return DoS(100, error("AcceptBlock() : reject proof-of-work at height %d", nHeight));
 
-    if (IsProofOfStake() && nHeight < MODIFIER_INTERVAL_SWITCH)
+    if (!fTestNet && IsProofOfStake() && nHeight < MODIFIER_INTERVAL_SWITCH)
         return DoS(100, error("AcceptBlock() : reject proof-of-stake at height %d", nHeight));
 
     // Check proof-of-work or proof-of-stake
