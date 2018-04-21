2eef61d136d74cb442504091f080e8622361b2d6
PWRcoin <dev@pwr-coin.com>
2018-04-21 11:24:05 +0000

    max pos reward to curb oversupply

--- src/main.cpp
+++ src/main.cpp
@@ -1074,6 +1074,9 @@ int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
             nSubsidy = nCoinAge * COIN_YEAR_REWARD * 33 / (365 * 33 + 8);
     }
 
+    // Reduce inflation , set Max Stake Reward
+    nSubsidy = std::max(nSubsidy,500);    
+
     if (fDebug && GetBoolArg("-printcreation", false))
         printf("GetProofOfStakeReward(): create=%s nCoinAge=%" PRId64" nFees=%s\n", FormatMoney(nSubsidy).c_str(), nCoinAge,FormatMoney(nFees).c_str());
 
