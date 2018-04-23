44ec0ae4c6dfe99e0102d3698152dbe8b236172e
PWRcoin <dev@pwr-coin.com>
2018-04-23 09:50:39 +0200

    adding release tags

--- src/clientversion.h
+++ src/clientversion.h
@@ -5,6 +5,8 @@
 // client versioning
 //
 
+#define BUILD_DATE "2018-04-23 08:29:53 +0200"
+
 // These need to be macros, as version.cpp's and pwrcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       2
 #define CLIENT_VERSION_MINOR       0
