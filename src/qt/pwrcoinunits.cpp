2683aa6efbca2b3e9927430ff74c1b92c1f74edf
PWRcoin <dev@pwr-coin.com>
2018-06-16 10:20:49 +0200

    adding new format() function to handle unsigned amount

--- src/qt/pwrcoinunits.cpp
+++ src/qt/pwrcoinunits.cpp
@@ -52,7 +52,7 @@ QString pwrcoinUnits::description(int unit)
     }
 }
 
-quint64 pwrcoinUnits::factor(int unit)
+qint64 pwrcoinUnits::factor(int unit)
 {
     switch(unit)
     {
@@ -91,11 +91,11 @@ QString pwrcoinUnits::format(int unit, quint64 n, bool fPlus)
     // localized number formatting.
     if(!valid(unit))
         return QString(); // Refuse to format invalid unit
-    quint64 coin = factor(unit);
+    qint64 coin = factor(unit);
     int num_decimals = decimals(unit);
     quint64 n_abs = (n > 0 ? n : -n);
     quint64 quotient = n_abs / coin;
-    quint64 remainder = n_abs % coin;
+    qint64 remainder = n_abs % coin;
     QString quotient_str = QString::number(quotient);
     QString remainder_str = QString::number(remainder).rightJustified(num_decimals, '0');
 
@@ -112,12 +112,44 @@ QString pwrcoinUnits::format(int unit, quint64 n, bool fPlus)
     return quotient_str + QString(".") + remainder_str;
 }
 
+QString pwrcoinUnits::format(int unit, qint64 n, bool fPlus)
+{
+    // Note: not using straight sprintf here because we do NOT want
+    // localized number formatting.
+    if(!valid(unit))
+        return QString(); // Refuse to format invalid unit
+    qint64 coin = factor(unit);
+    int num_decimals = decimals(unit);
+    qint64 n_abs = (n > 0 ? n : -n);
+    qint64 quotient = n_abs / coin;
+    qint64 remainder = n_abs % coin;
+    QString quotient_str = QString::number(quotient);
+    QString remainder_str = QString::number(remainder).rightJustified(num_decimals, '0');
+
+    // Right-trim excess zeros after the decimal point
+    int nTrim = 0;
+    for (int i = remainder_str.size()-1; i>=2 && (remainder_str.at(i) == '0'); --i)
+        ++nTrim;
+    remainder_str.chop(nTrim);
+
+    if (n < 0)
+        quotient_str.insert(0, '-');
+    else if (fPlus && n > 0)
+        quotient_str.insert(0, '+');
+    return quotient_str + QString(".") + remainder_str;
+}
+
+QString pwrcoinUnits::formatWithUnit(int unit, qint64 amount, bool plussign)
+{
+    return format(unit, amount, plussign) + QString(" ") + name(unit);
+}
+
 QString pwrcoinUnits::formatWithUnit(int unit, quint64 amount, bool plussign)
 {
     return format(unit, amount, plussign) + QString(" ") + name(unit);
 }
 
-bool pwrcoinUnits::parse(int unit, const QString &value, quint64 *val_out)
+bool pwrcoinUnits::parse(int unit, const QString &value, qint64 *val_out)
 {
     if(!valid(unit) || value.isEmpty())
         return false; // Refuse to parse invalid unit or empty string
--- src/qt/pwrcoinunits.h
+++ src/qt/pwrcoinunits.h
@@ -35,17 +35,21 @@ public:
     //! Longer description
     static QString description(int unit);
     //! Number of Satoshis (1e-8) per unit
-    static quint64 factor(int unit);
+    static qint64 factor(int unit);
     //! Number of amount digits (to represent max number of coins)
     static int amountDigits(int unit);
     //! Number of decimals left
     static int decimals(int unit);
     //! Format as string
+    static QString format(int unit, qint64 amount, bool plussign=false);
+    //! Format as string (with unit)
+    static QString formatWithUnit(int unit, qint64 amount, bool plussign=false);
+    //! Format as string
     static QString format(int unit, quint64 amount, bool plussign=false);
     //! Format as string (with unit)
     static QString formatWithUnit(int unit, quint64 amount, bool plussign=false);
     //! Parse string to coin amount
-    static bool parse(int unit, const QString &value, quint64 *val_out);
+    static bool parse(int unit, const QString &value, qint64 *val_out);
     ///@}
 
     //! @name AbstractListModel implementation
