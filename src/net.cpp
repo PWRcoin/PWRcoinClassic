2e50138cc055d4dd128e4613eaf2204b4b248ce0
ghobson2013 <ugac007@gmail.com>
2018-06-18 01:57:41 -0500

    removing old seeds, putting temp new vps for now

--- src/net.cpp
+++ src/net.cpp
@@ -1140,9 +1140,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-        {"seed1.pwr-coin.com", "seed1.pwr-coin.com"},
-        {"blockexplorer.pwr-coin.com", "blockexplorer.pwr-coin.com"},
-        {"138.68.27.117", "138.68.27.117"},
+        {"167.99.131.65", "167.99.131.65"},
         {"", ""}
 };
 
